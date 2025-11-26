7456b132e87e2ac715674788bf36f360e39a962c
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-11-26 14:18:29 -0500

    added so-called "tests"

---
+++ src/test/java/use_case/TextEntryInteractorTest.java
@@ -0,0 +1,17 @@
+package use_case;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TextEntryInteractorTest {
+    @Test
+    void isGood() {
+        assertTrue(true);
+    }
+
+    @Test
+    void isBad() {
+        fail();
+    }
+}
